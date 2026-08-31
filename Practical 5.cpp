6e14dae75f80e863bda3f75d119715b0f44a120a
sruthi <mamidipalli.varshitha131909@marwadiuniversity.ac.in>
2026-08-31 11:30:38 +0530

    Add files via upload

---
+++ Practical 5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int knapsack(int W, int wt[], int val[], int n)
+{
+    int dp[n + 1][W + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                );
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main()
+{
+    int n, W;
+
+    cin >> n;
+
+    int wt[n], val[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> wt[i];
+
+    for (int i = 0; i < n; i++)
+        cin >> val[i];
+
+    cin >> W;
+
+    cout << knapsack(W, wt, val, n);
+
+    return 0;
+}
